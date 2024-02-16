a59a2a8fcfc8068d90de3ad94e9f1a73cf761293
halenurkar <halenurkr@gmail.com>
2024-02-16 10:52:08 +0300

    add json-server

--- README.md
+++ README.md
@@ -14,6 +14,12 @@ Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 The page will reload when you make changes.\
 You may also see any lint errors in the console.
 
+### `npm install json-server`
+
+### `npx json-server --watch api/db.json --3004`
+
+### `npm install axios`
+
 ### `npm test`
 
 Launches the test runner in the interactive watch mode.\
--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import './App.css';
 import TaskCreate from './components/TaskCreate';
 import TaskList from './components/TaskList';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 function App() {
   const [tasks, setTasks] = useState([])
-  const createTask =(title, taskDesc)=>{
-  const createdTask=[
-    ...tasks,{
-      id:Math.round(Math.random()*999999),
+  const createTask = async(title, taskDesc)=>{
+
+    const response= await axios.post('http://localhost:3004/task',{
       title,
       taskDesc
-    }
+    });
+    console.log(response);
+  const createdTask=[
+    ...tasks,
+      response.data    
   ];
   setTasks(createdTask);
   };
 
-  const deleteTaskByID=(id)=>{
+  const fetchTask= async()=>{
+    const response= await axios.get('http://localhost:3004/task');
+    setTasks(response.data);
+  }
+
+  useEffect(()=>{
+    fetchTask();
+  },[])
+
+  const deleteTaskByID=async(id)=>{
+    await axios.delete(`http://localhost:3004/task/${id}`);
    const afterDeleteTaks= tasks.filter((tasks)=>{
       return tasks.id !== id;
     })
     setTasks(afterDeleteTaks);
   };
 
-  const editTaskById=(id, updatedTitle, updatedTaskDesc)=>{
+  const editTaskById=async(id, updatedTitle, updatedTaskDesc)=>{
+    await axios.put(`http://localhost:3004/task/${id}`,{
+      title:updatedTitle,
+      taskDesc:updatedTaskDesc
+    });
     const updatedTask = tasks.map((tasks)=>{
       if(tasks.id === id){
         return {id, title:updatedTitle, taskDesc:updatedTaskDesc}
